b11cb748a25eeb29226f3e00453cb4528cd039b0
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-03-22 01:04:13 -0300

    Initial Commit

---
+++ Gamehub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gamehub
+//
+//  Created by Marcello Gonzatto Birkan on 22/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gamehub/GamehubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamehubApp.swift
+//  Gamehub
+//
+//  Created by Marcello Gonzatto Birkan on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GamehubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
